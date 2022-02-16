af587c4986a067812232b3322fa6b37a39cdba3b
Xing Jin (IFAG IT DSA RD PE) <Jin.Xing@infineon.com>
2022-02-16 14:42:42 +0100

    attach header while proxy

--- echo.go
+++ echo.go
@@ -19,30 +19,6 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	//e.Pre(middleware.NonWWWRedirect())
-	//e.Pre(middleware.WWWRedirect())
-	//e.Pre(middleware.NonWWWRedirect())
-
-	// Setup proxy
-	/*
-		url1, err := url.Parse("http://localhost:8080")
-		if err != nil {
-			e.Logger.Fatal(err)
-		}
-		url2, err := url.Parse("http://localhost:4200")
-		if err != nil {
-			e.Logger.Fatal(err)
-		}
-		targets := []*middleware.ProxyTarget{
-			{
-				URL: url1,
-			},
-			{
-				URL: url2,
-			},
-		}
-		e.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
-	*/
 
 	// Routes
 	e.GET("/", sidecar)
@@ -72,16 +48,18 @@ func sidecar(c echo.Context) error {
 			// TODO: GAM endpoint to get claims
 			// TODO: cache to prevent from calling GAM all the time
 			token = JWT()
-			// TODO: Attach header with the redirect
-			//c.Redirect(302, "http://localhost:4200")
+			bearerToken := "Bearer " + token
 			req.Host = url2.Host
 			req.URL.Host = url2.Host
 			req.URL.Scheme = url2.Scheme
+			//Attach header with the redirect
+			res.Header().Add("Authorzaition", bearerToken)
 			proxy2.ServeHTTP(res, req)
 		} else {
 			//JWT token is present
 			fmt.Printf("JWT Authorization header provided! \n")
 			//Decode the JWT token
+			bearerToken := authHeader
 			authHeader = strings.Replace(authHeader, "Bearer ", "", 1)
 			claims := jwt.MapClaims{}
 			_, err := jwt.ParseWithClaims(authHeader, claims, func(token *jwt.Token) (interface{}, error) {
@@ -93,22 +71,16 @@ func sidecar(c echo.Context) error {
 						// The JWT token is issued by the basic auth sidecar
 						fmt.Println("Dealing with JWT token issued by the basic auth sidecar! the issuer is: ", val)
 						// TODO: verify the JWT token
-						// 		 attach header with the redirect
-						//c.Redirect(302, "http://localhost:4200")
 						req.Host = url2.Host
 						req.URL.Host = url2.Host
 						req.URL.Scheme = url2.Scheme
+						//Attach header with the redirect
+						res.Header().Add("Authorzaition", bearerToken)
 						proxy2.ServeHTTP(res, req)
 					} else {
 						fmt.Println("Dealing with JWT token issued by the iam sidecar! The issuer is: ", val, "The request will be redirected to the iam side car.")
 						// TODO: append JWT to the header of the request
 						c.Redirect(302, "http://localhost:8080")
-						/*
-							req.Host = url1.Host
-							req.URL.Host = url1.Host
-							req.URL.Scheme = url1.Scheme
-							proxy1.ServeHTTP(res, req)
-						*/
 					}
 				} else {
 					fmt.Println("No Issuer provided!")
